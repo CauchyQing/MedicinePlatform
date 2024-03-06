66d6cb9afae110285183c817faa8faa263dd84b6
卿柯序 <1415708632@qq.com>
2024-03-06 14:35:35 +0800

    qkx

--- src/main/java/org/qingkexu/pojo/dto/HospitalDTO.java
+++
@@ -1,8 +0,0 @@
-package org.qingkexu.pojo.dto;
-
-import lombok.Data;
-
-@Data
-public class HospitalDTO {
-    private Long userId;
-}
--- src/main/java/org/qingkexu/pojo/dto/ResthomeDTO.java
+++
@@ -1,8 +0,0 @@
-package org.qingkexu.pojo.dto;
-
-import lombok.Data;
-
-@Data
-public class ResthomeDTO {
-    private Long userId;
-}
